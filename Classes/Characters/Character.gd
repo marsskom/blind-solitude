9bbf36a2530d66aaeb9be8f1b04cfd6ef465c674
Andrii Prakapas <marsskom@gmail.com>
2022-05-13 09:22:19 +0300

    feat(basics): update state, create states collection, create basic charactes class

---
+++ Classes/Characters/Character.gd
@@ -0,0 +1,41 @@
+extends KinematicBody2D
+class_name Character
+
+signal vector_changed
+
+export(int) var ACCELERATION: int = 500
+export(int) var MAX_SPEED: int = 80
+export(int) var FRICTION: int = 500
+
+export(NodePath) var states_collection_node: NodePath
+
+var __stateMachine: StateMachine
+var __velocity: Vector2 = Vector2.DOWN setget set_velocity, get_velocity
+var __last_vector: Vector2 = Vector2.DOWN
+
+onready var states: StatesCollection = get_node(states_collection_node) as StatesCollection
+
+func _ready():
+	self.__stateMachine = StateMachine.new(states.get("Idle"))
+
+func set_velocity(velocity: Vector2) -> void:
+	__velocity = velocity
+
+func get_velocity() -> Vector2:
+	return __velocity
+
+func idle_process(delta) -> void:
+	self.__stateMachine.change(states.get("Idle"))
+
+	self.set_velocity(
+		self.get_velocity().move_toward(self.__last_vector, FRICTION * delta)
+	)
+
+func move_process(vector: Vector2, delta) -> void:
+	self.__stateMachine.change(states.get("Move"))
+
+	self.set_velocity(
+		self.get_velocity().move_toward(vector * MAX_SPEED, ACCELERATION * delta)
+	)
+
+	self.set_velocity(move_and_slide(self.get_velocity()))
