fe1833f4ef1d312a898e9f8f9fc0d0aace6aa46e
Andrii Prakapas <marsskom@gmail.com>
2022-05-14 14:46:49 +0300

    feat(basics): add tree, add collision hurtbox, set right window size

--- Scene/World/Weather/Components/DayNightCycle.gd
+++ Scene/World/Weather/Components/DayNightCycle.gd
@@ -5,5 +5,5 @@ onready var animation_player: AnimationPlayer = $AnimationPlayer
 func _process(delta):
 	var dict = TimeSystem.formed_datetime(OS.get_ticks_msec())
 
-	animation_player.play("DayNightCycle")
-	animation_player.seek(dict.hours as float)
+#	animation_player.play("DayNightCycle")
+#	animation_player.seek(dict.hours as float)
